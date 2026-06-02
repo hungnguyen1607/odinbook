d1f363bc32ec6682d62d8969194e116ab56907c0
hungnguyen1607 <h7ng.ng@gmail.com>
2026-06-02 16:21:55 +0700

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,6 @@ class UsersController < ApplicationController
   def index
     @users = User.where.not(id: current_user.id)
     @pending_sent = current_user.sent_requests.pluck(:receiver_id)
+    @friends = current_user.friends + current_user.friends_received
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,13 @@ class User < ApplicationRecord
   has_many :sent_requests, class_name: "Friendship", foreign_key: :requester_id, dependent: :destroy
   has_many :received_requests, class_name: "Friendship", foreign_key: :receiver_id, dependent: :destroy
 
-
   has_many :friends, -> { where(friendships: { status: "accepted" }) },
             through: :sent_requests, source: :receiver
   
-  after_create :send_welcome_email
+  
+  has_many :friends_received, -> { where(friendships: { status: "accepted" }) },
+            through: :received_requests, source: :requester
+
 
   private
 
