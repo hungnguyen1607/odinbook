38058ad088f4f28923604496a0738d5a5a759769
hungnguyen1607 <h7ng.ng@gmail.com>
2026-06-04 15:15:35 +0700

    hi

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,13 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
-    @posts = @users.posts.order(created_at: :desc)
+    @posts = @user.posts.order(created_at: :desc)
   end
 
   def index
     @users = User.where.not(id: current_user.id)
     @pending_sent = current_user.sent_requests.pluck(:receiver_id)
     @friends = current_user.friends + current_user.friends_received
+
   end
 end
